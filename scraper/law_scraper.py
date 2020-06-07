1d9c1b2d25a42a3dc7e5a8bf307fb4d4289df50d
Deniz Bozyigit <dbozyigit@battrion.com>
2020-06-07 19:44:26 +0200

    current version 200607

---
+++ scraper/law_scraper.py
@@ -0,0 +1,43 @@
+# from https://realpython.com/python-web-scraping-practical-introduction/
+
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
+
+import logging
+
+module_logger = logging.getLogger(__name__)
+module_logger.setLevel(logging.INFO)
+
+
+def simple_get(url):
+    """
+    Attempts to get the content at `url` by making an HTTP GET request.
+    If the content-type of response is some kind of HTML/XML, return the
+    text content, otherwise return None.
+    """
+    try:
+        with closing(get(url, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+
+    except RequestException as e:
+        module_logger.error('Error during requests to {0} : {1}'.format(url, str(e)))
+        return None
+
+
+def is_good_response(resp):
+    """
+    Returns True if the response seems to be HTML, False otherwise.
+    """
+    content_type = resp.headers['Content-Type'].lower()
+    return (resp.status_code == 200 
+            and content_type is not None 
+            and content_type.find('html') > -1)
+
+
+
+
